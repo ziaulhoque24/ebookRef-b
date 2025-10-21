8595912d5ec58c526767f628f4844f0f42b98acd
Ziaul Hoque <ziaul.hoque124@gmail.com>
2025-10-21 18:32:40 +0600

    feat: enhance user registration with unique referral code generation and referral tracking
    - Design database schema for ebook, order and cart also modified previous user schema to manage referral system and added Referral schema

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,31 @@
 import bcrypt from "bcryptjs";
+import * as crypto from "crypto";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma";
 import { LoginInput, RegisterInput } from "../schemas/auth.schema";
+
+const generateUniqueReferralCode = async (
+  firstName: string
+): Promise<string> => {
+  let newReferralCode: string;
+  let isUnique = false;
+
+  do {
+    const randomChars = crypto.randomBytes(2).toString("hex").toUpperCase();
+    newReferralCode = `${firstName
+      .toUpperCase()
+      .substring(0, 4)}${randomChars}`;
+    const existingUser = await prisma.user.findUnique({
+      where: { referralCode: newReferralCode },
+    });
+    if (!existingUser) {
+      isUnique = true;
+    }
+  } while (!isUnique);
+
+  return newReferralCode;
+};
 
 export const registerUser = async (
   req: Request<{}, {}, RegisterInput>,
@@ -18,19 +41,37 @@ export const registerUser = async (
         .json({ message: "User with this email already exists." });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const referrer = referralCode
+      ? await prisma.user.findUnique({ where: { referralCode } })
+      : null;
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-        firstName,
-        lastName,
-        referralCode: referralCode,
-      },
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUserReferralCode = await generateUniqueReferralCode(firstName);
+
+    const newUser = await prisma.$transaction(async (tx) => {
+      const createdUser = await tx.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          firstName,
+          lastName,
+          referralCode: newUserReferralCode,
+        },
+      });
+
+      if (referrer) {
+        await tx.referral.create({
+          data: {
+            referrerId: referrer.id,
+            referredUserId: createdUser.id,
+          },
+        });
+      }
+
+      return createdUser;
     });
 
-    const { password: _, ...userWithoutPassword } = user;
+    const { password: _, ...userWithoutPassword } = newUser;
     res.status(201).json(userWithoutPassword);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
